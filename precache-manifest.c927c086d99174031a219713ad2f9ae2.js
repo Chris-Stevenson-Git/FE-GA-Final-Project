5303785706fb4605cc492acbce1c4b151580ebb5
Chris-Stevenson-Git <christopher-stevenson@outlook.com>
2020-09-25 09:45:36 +1000

    Updates

---
+++ precache-manifest.c927c086d99174031a219713ad2f9ae2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afce438cadd0ef3a3018b4b84c5d07e1",
+    "url": "/FE-GA-Final-Project/index.html"
+  },
+  {
+    "revision": "8d6681820b7f263a3e49",
+    "url": "/FE-GA-Final-Project/static/css/main.7b06a0ea.chunk.css"
+  },
+  {
+    "revision": "2d14db22a1fa6b5e5ee3",
+    "url": "/FE-GA-Final-Project/static/js/2.b346a42f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FE-GA-Final-Project/static/js/2.b346a42f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d6681820b7f263a3e49",
+    "url": "/FE-GA-Final-Project/static/js/main.d962a375.chunk.js"
+  },
+  {
+    "revision": "4855aa5f600a289e7307",
+    "url": "/FE-GA-Final-Project/static/js/runtime-main.9f764c28.js"
+  }
+]);
